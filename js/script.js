a3ec80c94ed7bf9095e9678c04b5aaeafaac9efc
Josuelee <josuelee28@gmail.com>
2022-07-27 20:48:29 -0600

    hover in projects and show details added, also projects images changes

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
-import { globalProjects, globalSkills, myEmail, projectsTechnologies } from "./globalData.min.js"
+import { globalProjects, globalSkills, myEmail, projectsTechnologies } from "./globalData.min.js";
 
 const doc = document;
 const sections = doc.querySelectorAll("[data-scroll-spy]");
-
 /* Fixed menu */
 
 const callback = (entries) => {
+  entries.forEach((entry) => {
+    const sectionId = entry.target.id;
 
-  entries.forEach(( entry ) => {    
-    const sectionId = entry.target.id;      
-    
-    if(entry.isIntersecting){                  
-      doc.querySelector(`[data-scroll-id="${sectionId}"]`).classList.add("fixed-navbar__link--active");      
+    if (entry.isIntersecting) {
+      doc
+        .querySelector(`[data-scroll-id="${sectionId}"]`)
+        .classList.add("fixed-navbar__link--active");
       doc.getElementById(sectionId).classList.add("section-active");
     } else {
-      doc.querySelector(`[data-scroll-id="${sectionId}"]`).classList.remove("fixed-navbar__link--active");      
+      doc
+        .querySelector(`[data-scroll-id="${sectionId}"]`)
+        .classList.remove("fixed-navbar__link--active");
       doc.getElementById(sectionId).classList.remove("section-active");
     }
-
-  })  
-}
+  });
+};
 
 const io = new IntersectionObserver(callback, {
-  threshold: .1
+  threshold: 0.1,
 });
 
-sections.forEach(( section ) => io.observe(section) );
-
+sections.forEach((section) => io.observe(section));
 
 /* Projects y skills */
 
@@ -35,8 +35,7 @@ const projectsArticle = doc.getElementById("project-container");
 const skillsContainer = doc.getElementById("skillsContainer");
 
 const getAllCategories = (projects = []) => {
-  
-  const projectData = projects.map(( project ) => {    
+  const projectData = projects.map((project) => {
     return `
       <article class="projects-section__article">
         <div class="projects-section__article-container">
@@ -57,24 +56,30 @@ const getAllCategories = (projects = []) => {
             <div class="projects-section__type">${project.categoryText}</div>
           </div>
           <div class="projects-section__information">
-            <h3>${project.title}</h3>
-            <p>${project.text}</p>            
-            <div class="projects-section__technologies">
+            <div class="projects-section__information-container">
+              <h3 class="projects-section__information_title">
+                ${project.title}
+              </h3>              
+            </div>
+            <div class="projects-section__details">                                    
+              <p>${project.text}</p>            
+              <div class="projects-section__technologies"></div>              
             </div>
           </div>
         </div>
       </article>
-    `
+    `;
   });
-  
-  projectsArticle.innerHTML = projectData.join("");        
-}
 
-const getCategories = (projects = [], id = "") => {  
+  projectsArticle.innerHTML = projectData.join("");
+};
 
-  const projectsFiltered = projects.filter(( project ) => project.category === id);
+const getCategories = (projects = [], id = "") => {
+  const projectsFiltered = projects.filter(
+    (project) => project.category === id
+  );
 
-   const projectData = projectsFiltered.map(( project ) => {    
+  const projectData = projectsFiltered.map((project) => {
     return `
       <article class="projects-section__article">
         <div class="projects-section__article-container">
@@ -92,25 +97,31 @@ const getCategories = (projects = [], id = "") => {
                 </a>
               </div>
             </div>
-            <div class="projects-section__type">${project.categoryText}</div>
+            <div class="projects-section__type">
+              ${project.categoryText}
+            </div>
           </div>
           <div class="projects-section__information">
-            <h3>${project.title}</h3>
-            <p>${project.text}</p>
-            <div class="projects-section__technologies">
+            <div class="projects-section__information-container">
+              <h3 class="projects-section__information_title">
+                ${project.title}
+              </h3>                            
+            </div>
+            <div class="projects-section__details">                            
+              <p>${project.text}</p>            
+              <div class="projects-section__technologies"></div>              
             </div>
           </div>
         </div>
       </article>
-    `
-  })
-    
-  projectsArticle.innerHTML = projectData.join("");      
+    `;
+  });
 
-}
+  projectsArticle.innerHTML = projectData.join("");
+};
 
 const getSkills = (skills = []) => {
-  const skillsData = skills.map(( skill ) => {
+  const skillsData = skills.map((skill) => {
     return `
       <article class="skills-section__article">
         <div class="skills-section__image ${skill.skillColor}">
@@ -118,141 +129,145 @@ const getSkills = (skills = []) => {
         </div>
         <p class="skills-section__text">${skill.text}</p>
       </article>
-    `
+    `;
   });
 
-  skillsContainer.innerHTML = skillsData.join("")
-  
-}
+  skillsContainer.innerHTML = skillsData.join("");
+};
 
 const getTechnologies = () => {
-  const projectsArticles = doc.querySelectorAll(".projects-section__article");      
+  const projectsArticles = doc.querySelectorAll(".projects-section__article");
 
-  projectsArticles.forEach(project_article => {
+  projectsArticles.forEach((project_article) => {
     const imgAlt = project_article.querySelector(".projects-section__image-item").alt;
-    const tech = projectsTechnologies.find( el => el.altRef === imgAlt);
+    const tech = projectsTechnologies.find((el) => el.altRef === imgAlt);
 
     if (imgAlt === tech.altRef) {
+      const technologiesParagraph = tech.technologies.map(
+        (tech) => `<li>${tech}</li>`
+      );
 
-      const technologiesParagraph = tech.technologies.map(tech => ( 
-          `<li>${tech}</li>` 
-        ))
+      const sectionTechnologies = project_article.querySelector(
+        ".projects-section__technologies"
+      );
+      sectionTechnologies.innerHTML = technologiesParagraph.join("");
+    }
+  });
+};
 
-      const sectionTechnologies = project_article.querySelector(".projects-section__technologies");
-      sectionTechnologies.innerHTML= technologiesParagraph.join("");
-    }    
-  })
-}
 
-doc.addEventListener("DOMContentLoaded" , () => {
+doc.addEventListener("DOMContentLoaded", () => {
   getAllCategories(globalProjects);
-  getSkills(globalSkills); 
-  getTechnologies(); 
-})
+  getSkills(globalSkills);
+  getTechnologies();
+});
 
-doc.addEventListener("click" , (e) => {
-  if(e.target.matches(".projects-section__category")){
+doc.addEventListener("click", (e) => {
+  if (e.target.matches(".projects-section__category")) {
     const id = e.target.id;
 
-    if(id === "all"){
+    if (id === "all") {
       getAllCategories(globalProjects);
       projectsSection.classList.remove("projects-section--category");
       getTechnologies();
     } else {
-      getCategories(globalProjects, id);    
+      getCategories(globalProjects, id);
       projectsSection.classList.add("projects-section--category");
       getTechnologies();
     }
-
-  }
-})
+    
+  } 
+});
 
 
 /* Form Contact*/
 
-
 const form = doc.getElementById("form");
 const inputs = doc.querySelectorAll(".contact-section__form [required] ");
 
-inputs.forEach(( input ) => {
+inputs.forEach((input) => {
   const span = doc.createElement("span");
   span.id = input.name;
-  span.textContent = input.title;  
+  span.textContent = input.title;
   span.classList.add("contact-section__validation");
-  input.insertAdjacentElement("afterend", span)
-})
-
+  input.insertAdjacentElement("afterend", span);
+});
 
 const keyDownMethod = (input) => {
   const validatorElement = doc.getElementById(input.name);
   const pattern = input.pattern || input.dataset.pattern;
-  
-  const reg = new RegExp(pattern, "ig")      
 
-  if (!validatorElement) return;    
+  const reg = new RegExp(pattern, "ig");
 
-  return (!reg.test(input.value) && input.value !== "" || input.value.length > 255) 
+  if (!validatorElement) return;
+
+  return (!reg.test(input.value) && input.value !== "") ||
+    input.value.length > 255
     ? validatorElement.classList.add("contact-section__validation--activate")
-    : validatorElement.classList.remove("contact-section__validation--activate")  
-    
-}
+    : validatorElement.classList.remove(
+        "contact-section__validation--activate"
+      );
+};
 
-form.addEventListener("keyup" , (e) => {
-  keyDownMethod(e.target);  
-})
+form.addEventListener("keyup", (e) => {
+  keyDownMethod(e.target);
+});
 
-
-const formResponseMethod = (responseMessage, message, loader ) => {      
-
+const formResponseMethod = (responseMessage, message, loader) => {
   loader.classList.remove("contact-section__response--active");
-  responseMessage.textContent =  message;        
+  responseMessage.textContent = message;
   responseMessage.classList.add("contact-section__response--active");
-  
-  setTimeout(() => {                
+
+  setTimeout(() => {
     responseMessage.classList.remove("contact-section__response--active");
     form.reset();
-  }, 3500);  
-}
+  }, 3500);
+};
 
 const sendSubmit = async (form) => {
   const responseMessage = doc.getElementById("form-response");
   const loader = doc.getElementById("loader");
 
   try {
-    const formData = new FormData(form);  
+    const formData = new FormData(form);
 
     const options = {
       method: "POST",
-      body: formData,      
-    }
+      body: formData,
+    };
 
-    loader.classList.add("contact-section__response--active");      
+    loader.classList.add("contact-section__response--active");
 
     const res = await fetch(`https://formsubmit.co/ajax/${myEmail}`, options);
     const json = await res.json();
 
     if (res.ok && json.success === "true") {
-      formResponseMethod(responseMessage,"The message was submitted successfully 😎", loader);      
+      formResponseMethod(
+        responseMessage,
+        "The message was submitted successfully 😎",
+        loader
+      );
     } else {
-      formResponseMethod(responseMessage, "Something wrong happened! 🙁", loader);      
+      formResponseMethod(
+        responseMessage,
+        "Something wrong happened! 🙁",
+        loader
+      );
     }
-
   } catch (error) {
-    formResponseMethod(responseMessage, "Something wrong happened! 🙁", loader);      
+    formResponseMethod(responseMessage, "Something wrong happened! 🙁", loader);
   }
-  
-}
+};
 
-form.addEventListener("submit" , (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-      
+
   if (
-      form.message.value.length > 255 ||
-      form.name.value.length > 255 ||
-      form.subject.value.length > 255 
-    ) return;
+    form.message.value.length > 255 ||
+    form.name.value.length > 255 ||
+    form.subject.value.length > 255
+  )
+    return;
 
   sendSubmit(e.target);
-})
-
-
+});
